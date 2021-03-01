889165ef744425304b39838d39d60a88ed1c89ae
Artur Melo Catunda <arturmcatunda@gmail.com>
2021-03-01 18:09:49 -0300

    Update MainWindow.java
    Primeira Versão da GUI.

--- src/main/java/UI/MainWindow.java
+++ src/main/java/UI/MainWindow.java
@@ -1,5 +1,56 @@
 package UI;
 
-public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class MainWindow implements ActionListener{
+		
+	private JButton loadButton = new JButton("Carregar");
+	private JProgressBar loadProgressBar = new JProgressBar();
+
+	private JLabel collisionLabel = new JLabel("Colisões:", JLabel.CENTER);
+	private JLabel collisionCounterLabel = new JLabel("0", JLabel.CENTER);
+
+	private JLabel overflowLabel = new JLabel("Overflows:", JLabel.CENTER);
+	private JLabel overflowCounterLabel = new JLabel("0", JLabel.CENTER);
+	
+	private JTextField searchTextField = new JTextField();
+	private JButton searchButton = new JButton("Buscar");	
+
+	private JLabel acessLabel = new JLabel("Acessos:", JLabel.CENTER);
+	private JLabel acessCounterLabel = new JLabel("0", JLabel.CENTER);
+	
+	public gui() {
+		
+		 JFrame frame = new JFrame("Projeto de Tabela Hash");
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 //frame.setSize(400, 400);
+		 frame.getContentPane().setLayout(new GridLayout(5,2,10,10));
+
+		loadProgressBar.setStringPainted(true);
+
+		 frame.getContentPane().add(loadButton);
+		 frame.getContentPane().add(loadProgressBar);
+		 frame.getContentPane().add(collisionLabel);
+		 frame.getContentPane().add(collisionCounterLabel);
+		 frame.getContentPane().add(overflowLabel);
+		 frame.getContentPane().add(overflowCounterLabel);
+		 frame.getContentPane().add(searchTextField);
+		 frame.getContentPane().add(searchButton);
+		 frame.getContentPane().add(acessLabel);
+		 frame.getContentPane().add(acessCounterLabel);
+		 
+		 //button.addActionListener(this);
+		 
+		 frame.pack();
+	     frame.setVisible(true);
+	     frame.setResizable(false);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 }
