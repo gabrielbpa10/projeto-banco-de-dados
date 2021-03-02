d0eb35d350af434b85e129dda8d578d6e3b1bb82
Vinícius <smartprecast.nb2@gmail.com>
2021-03-02 22:44:02 +0000

    Initialized working table controller

--- src/main/java/Main/IndiceHash.java
+++ src/main/java/Main/IndiceHash.java
@@ -1,15 +1,20 @@
 package Main;
 
+import ReturnTypes.*;
 import UI.MainWindow;
+import controller.*;
 
 public class IndiceHash {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        TableController tc = new TableController((k) -> {
+            return k.length() % 6;
+        }, 6, 2, 3);
         MainWindow mw = new MainWindow();
         mw.gui();
     }
 
-    public static void log (Object obj) {
+    public static void log(Object obj) {
         System.out.println(obj.toString());
     }
 }
