b6148e600a1e2444ff66be0e50928c7381f26035
Vinícius <smartprecast.nb2@gmail.com>
2021-03-02 20:47:07 +0000

    Added system look and feel aware

--- src/main/java/UI/MainWindow.java
+++ src/main/java/UI/MainWindow.java
@@ -1,5 +1,21 @@
 package UI;
 
+import javax.swing.*;
+
 public class MainWindow {
 
+    public JFrame Frame = new JFrame();
+
+    public MainWindow () {
+        try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
+    public void run () {
+
+    }
 }
