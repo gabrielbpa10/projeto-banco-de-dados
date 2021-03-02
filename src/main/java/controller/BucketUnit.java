1eb2b6f5eb169179870843c9fe4e3d547d083ed4
Vinícius <smartprecast.nb2@gmail.com>
2021-03-02 22:45:31 +0000

    Added toString method and added null page validation

--- src/main/java/controller/BucketUnit.java
+++ src/main/java/controller/BucketUnit.java
@@ -5,9 +5,15 @@ public class BucketUnit {
 	String key;
 	Page pagePointer;
 	
-	public BucketUnit (String key, Page page) {
+	public BucketUnit (String key, Page page) throws Exception {
+		if (page == null) throw new Exception("Bucket unit cannot have a null page pointer. Key ("+key+").");
 		this.key = key;
 		this.pagePointer = page;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("BucketUnit([%s, %s])", key, (pagePointer == null ? "null" : "PagePointer"));
 	}
 	
 }
