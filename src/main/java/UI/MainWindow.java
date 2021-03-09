1d62b0ad1183927a48eef0ef9216ee27af0a00df
TimeLord2010 <smartprecast.nb2@gmail.com>
2021-03-09 21:17:00 +0000

    Added file reader

--- src/main/java/UI/MainWindow.java
+++ src/main/java/UI/MainWindow.java
@@ -1,13 +1,20 @@
 package UI;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+
+import controller.TableController;
+import controller.TextFileReader;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MainWindow implements ActionListener {
 
     public JFrame frame = new JFrame("Projeto de Tabela Hash");
+    TableController tableController;
 
     public MainWindow() {
         try {
@@ -16,6 +23,7 @@ public class MainWindow implements ActionListener {
             e.printStackTrace();
         }
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        tableController = new TableController((a) -> a.length() % 10, 10, 4, 3);
     }
 
     public void run() {
@@ -36,14 +44,35 @@ public class MainWindow implements ActionListener {
 
     private JLabel acessLabel = new JLabel("Acessos:", JLabel.CENTER);
     private JLabel acessCounterLabel = new JLabel("0", JLabel.CENTER);
-
     public void gui() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // frame.setSize(400, 400);
         frame.getContentPane().setLayout(new GridLayout(5, 2, 10, 10));
 
         loadProgressBar.setStringPainted(true);
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) throws Exception {  
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                int result = chooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File f = chooser.getSelectedFile();
+                    try {
+                        TextFileReader.ReadFile(f.getName(), (line) -> {
+                            try {
+                                tableController.insert(line);
+                            } catch (Exception e1) {
+                                e1.printStackTrace();
+                            }
+                        });
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
 
+        });
         frame.getContentPane().add(loadButton);
         frame.getContentPane().add(loadProgressBar);
         frame.getContentPane().add(collisionLabel);
